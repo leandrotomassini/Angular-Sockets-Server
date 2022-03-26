e066eb8b60fa9d93c6b50b3ce479ffbaf53530ae
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-03-26 16:59:59 -0300

    Servidor chat

--- classes/server.ts
+++ classes/server.ts
@@ -1,19 +1,60 @@
 import express from 'express';
+import socketIO from 'socket.io';
+import http from 'http';
 
 import { SERVER_PORT } from '../global/enviroment';
+import * as socket from '../sockets/socket';
 
 
 export default class Server {
 
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+
+    private constructor() {
+
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+
+        this.io = new socketIO.Server(this.httpServer, {
+            cors: {
+                origin: true,
+                credentials: true
+            }
+        });
+
+        this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets() {
+
+        console.log(`Escuchando conexiones - sockets`);
+
+        this.io.on('connection', cliente => {
+
+            console.log('Cliente conectado.');
+
+            // Mensajes
+            socket.mensaje(cliente, this.io);
+
+            // Desconectar
+            socket.desconectar(cliente);
+        });
     }
 
     start(callback: VoidFunction) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import router from "./routes/router";
 import bodyParser from "body-parser";
 import cors from 'cors';
 
-const server = new Server();
+// Singleton
+const server = Server.instance;
 
 // BodyParser
 server.app.use(bodyParser.urlencoded({ extended: true }));
