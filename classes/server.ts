91b71dc4402fb86aac0bfdce0a06d7b9fde7c100
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-05-20 17:37:42 -0300

    Manejo de la lista de usuarios vía sockets

--- classes/server.ts
+++ classes/server.ts
@@ -44,13 +44,21 @@ export default class Server {
 
         this.io.on('connection', cliente => {
 
-            console.log('Cliente conectado.');
+            // Conectar cliente
+            socket.conectarCliente(cliente, this.io);
+
+            // Configurar usuario
+            socket.configurarUsuario(cliente, this.io);
+
+            // Obtener usuarios activos
+            socket.obtenerUsuarios(cliente, this.io);
 
             // Mensajes
             socket.mensaje(cliente, this.io);
 
             // Desconectar
-            socket.desconectar(cliente);
+            socket.desconectar(cliente, this.io);
+
         });
     }
 
--- routes/router.ts
+++ routes/router.ts
@@ -2,6 +2,10 @@
 
 import { Router, Request, Response } from 'express';
 
+import Server from '../classes/server';
+import { usuariosConectados } from '../sockets/socket';
+
+
 const router = Router();
 
 router.get('/mensajes', (req: Request, res: Response) => {
@@ -18,6 +22,15 @@ router.post('/mensajes', (req: Request, res: Response) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
 
+    const payload = {
+        cuerpo,
+        de
+    }
+
+    const server = Server.instance;
+
+    server.io.emit('mensaje-nuevo', payload);
+
     res.json({
         ok: true,
         cuerpo,
@@ -31,11 +44,47 @@ router.post('/mensajes/:id', (req: Request, res: Response) => {
     const de = req.body.de;
     const id = req.params.id;
 
+    const payload = {
+        de,
+        cuerpo
+    }
+
+    const server = Server.instance;
+
+    server.io.in(id).emit('mensaje-privado', payload);
+
     res.json({
         ok: true,
         cuerpo,
         de,
         id
+    });
+});
+
+// Servicios para obtener los ids de todos los usuarios
+router.get('/usuarios', (req: Request, res: Response) => {
+
+    const server = Server.instance;
+
+    server.io.allSockets().then((clientes) => {
+        res.json({
+            ok: true,
+            clientes: Array.from(clientes)
+        });
+    }).catch((err) => {
+        res.json({
+            ok: false,
+            err
+        });
+    });
+});
+
+
+// Obtener usuarios y sus nombre
+router.get('/usuarios/detalle', (req: Request, res: Response) => {
+    res.json({
+        ok: true,
+        clientes: usuariosConectados.getLista()
     });
 });
 
