c378e3d4f35d49b854b4c1a220a99f36c0e56bd8
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-07-17 21:47:25 +0000

    Sección 5 completa enviar mensajes

--- 01-server/classes/server.ts
+++ 01-server/classes/server.ts
@@ -54,13 +54,18 @@ export default class Server {
 
         this.io.on('connection', cliente => {
 
-            console.log('Cliente conectado.');
+            // Conectar cliente
+            socket.conectarCliente(cliente);
+
+            // Configurar usaurio
+            socket.configurarUsuario(cliente, this.io);
 
             // Mensajes
             socket.mensaje(cliente, this.io);
 
             // Desconectar
             socket.desconectar(cliente);
+
 
         });
 
--- 01-server/routes/router.ts
+++ 01-server/routes/router.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express';
 
+import Server from '../classes/server';
+
 
 const router = Router();
 
@@ -16,6 +18,15 @@ router.post('/mensajes', (req: Request, resp: Response) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
 
+    const payload = {
+        de,
+        cuerpo
+    }
+
+    // Tiene la propiedad de socket.io para mandar mensajes
+    const server = Server.instance;
+    server.io.emit('mensaje-nuevo', payload);
+
     resp.status(200).json({
         ok: true,
         cuerpo,
@@ -28,6 +39,15 @@ router.post('/mensajes/:id', (req: Request, resp: Response) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
     const id = req.params.id;
+
+    const payload = {
+        de,
+        cuerpo
+    }
+
+    // Tiene la propiedad de socket.io para mandar mensajes
+    const server = Server.instance;
+    server.io.in(id).emit('mensaje-privado', payload);
 
     resp.status(200).json({
         ok: true,
